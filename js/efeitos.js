83f5c7d418d5611af6c4e8dfba434a3b6df83cf2
ldevLucasl <meistergames@gmail.com>
2020-03-31 17:07:05 -0300

    responsive-mobile

--- js/efeitos.js
+++ js/efeitos.js
@@ -26,4 +26,14 @@ $(document).ready(function(){
       }			
     }
   });
+  $("#btn-bars").on("click", function(){
+    $("header").toggleClass("open-menu");
+  });
+  $("#menu-mobile-mask, .btn-close").on("click", function(){
+    $("header").removeClass("open-menu");
+  });
+  $("#btn-search").on("click", function(){
+    $("header").toggleClass("open-search");
+    $("#input-search-mobile").focus();
+  });
 });
